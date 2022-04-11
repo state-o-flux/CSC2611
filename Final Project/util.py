c6083bbc384ce9b3ce9050dad8a20eea25e4ee35
state-o-flux <44091163+state-o-flux@users.noreply.github.com>
2022-04-11 13:57:49 -0400

    Update util.py

--- Final Project/util.py
+++ Final Project/util.py
@@ -1,6 +1,8 @@
 import sys
 import json
+import nltk
 import numpy as np
+import pandas as pd
 
 def load_data(file_name):
     # Loads all required packages and data
@@ -19,13 +21,17 @@ def load_data(file_name):
             with open(f'data/{file_name}.json', 'r') as file:
                 data = json.load(file)
         except:
-            None
+            sys.stdout.write('\r')    
+            sys.stdout.write(f'Loading {file_name} ...not found\n')
+            return
     elif 'word_degrees' in file_name:
         try:
             with open(f'data/{file_name}.json', 'r') as file:
                 data = json.load(file)
         except:
-            None
+            sys.stdout.write('\r')    
+            sys.stdout.write(f'Loading {file_name} ...not found\n')
+            return
     elif file_name == 'res':
         with open('data/res.json', 'r') as file:
             remove_words = json.load(file)
@@ -60,6 +66,7 @@ def unique(vec):
     return result
 
 def cosine_similarity(v1, v2):
+    # Computes cosine similarity between two vectors
     cos_sim = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     return cos_sim
 
@@ -74,10 +81,104 @@ def most_similar(word, kids_ppmi, adult_ppmi, top_n):
     return top_words
 
 def gen_z_score(vec):
+    # Scales a vector to the z-distribution
     z_score = [(i - np.mean(vec)) / np.std(vec) for i in vec]
     return z_score
 
+def gen_fdist(corpora, grades, save=False):
+    # Generates frequency distributions for all words in all corpora. Includes number of times a word was spoken and the number of speakers who spoke the word.
+    dfs = []
+    fdist_dict = {}
+    for i in grades:
+        print('Generating distributions for {}'.format(i))
+        corpus = corpora[i]
+        corpus_combined = ' '.join(corpus)
+
+        fdist = dict(nltk.FreqDist(nltk.word_tokenize(corpus_combined)))
+        fdist = dict(sorted(fdist.items(), key=lambda item: item[1], reverse=True))
+
+        words = list(fdist.keys())
+        n = len(words)
+        for j in range(n):
+            word = words[j]
+            speakers = 0
+            for k in corpus:
+                if word in nltk.word_tokenize(k):
+                    speakers += 1
+            fdist[word] = {'freq':fdist[word], 'speakers':speakers}
+            p = round(((j + 1) / n) * 100)
+            print(f"'{word}' done: {p}% complete")
+
+        fdist_df = []
+        for j in fdist.items():
+            fdist_df.append({'word':j[0],'freq':j[1]['freq'],'speakers':j[1]['speakers']})
+        fdist_df = pd.DataFrame(fdist_df)
+        fdist_df.columns = [j + '_' + i for j in fdist_df.columns]
+        dfs.append(fdist_df)
+        fdist_dict[i] = fdist
+
+    if save:
+        dfs = pd.concat(dfs, axis=1)
+        dfs.to_excel('fdist_addon.xlsx')
+
+        with open('fdist_addon.json', 'w') as file:
+            json.dump(fdist_dict, file)
+    
+    return fdist_dict
+
+def gen_sum_stats(type, save=False):
+    # Generates summary statistics regarding the frequency distributions including the number of tokens, types, types greater than 1, types greater than 2, speakers greater than 2, and total number of speakers per grade. 
+    corp_name = type + '_corpora'
+    fdist_name = 'fdist_' + type
+
+    adult_words = load_data('corpora_adult')
+    adult_words = adult_words['unique_words']
+    corpora = load_data(corp_name)
+    remove_words = load_data('res')
+    fdists = load_data(fdist_name)
+
+    grades = [i for i in fdists]
+    n_speakers = [len(corpora[i]) for i in grades]
+    tokens = [sum([fdists[i][j]['freq'] for j in fdists[i]]) for i in fdists]
+    types = [len(fdists[i]) for i in fdists]
+
+    type_greater1 = []
+    type_greater2 = []
+    speakers_greater1 = []
+    speakers_greater2 = []
+    for i in fdists:
+        x1 = 0
+        x2 = 0
+        x3 = 0
+        x4 = 0
+        for j in fdists[i]:
+            if j in adult_words and j not in remove_words:
+                if fdists[i][j]['freq'] > 1:
+                    x1 += 1
+                if fdists[i][j]['freq'] > 2:
+                    x2 += 1
+                if fdists[i][j]['speakers'] > 1:
+                    x3 += 1
+                if fdists[i][j]['speakers'] > 2:
+                    x4 += 1
+        type_greater1.append(x1)
+        type_greater2.append(x2)
+        speakers_greater1.append(x3)
+        speakers_greater2.append(x4)
+
+    df = pd.concat([pd.DataFrame(i) for i in [grades, tokens, types, type_greater1, speakers_greater1, type_greater2, speakers_greater2, n_speakers]], axis=1).T
+    cols = df.iloc[0]
+    df = df[1:]
+    df.columns = cols
+    df.index = ['tokens','types','types > 1', 'speakers > 1', 'types > 2', 'speakers > 2', 'n_speakers']
+
+    if save:
+        df.to_excel('data/fdist_sum_' + type + '.xlsx')
+    
+    return df
+
 def gen_word_groups(text, window):
+    # Function for grouping words that surround another in a corpus
     word_groups = []
     words = []
     for i, word in enumerate(text):
